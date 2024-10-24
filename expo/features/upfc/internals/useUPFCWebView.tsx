6a3abc54109d94d9e3881bb46bbb511012754791
yssk22 <yssk22@gmail.com>
2024-10-24 04:29:20 +0000

    [expo] fix minor issues in upfc.
    **Summary**
    
    fixed followings:
    
    - UPFCWebViewScreen did not follow the site in applications.
    - HomeTabUPFCCurrentApplicationList did not use `useHomeTabContext()` so unnecessary loading occured.
    
    **Test**
    
    - yarn test
    - expo
    
    **Issue**
    
    - N/A

--- expo/features/upfc/internals/useUPFCWebView.tsx
+++ expo/features/upfc/internals/useUPFCWebView.tsx
@@ -1,11 +1,11 @@
 import { useNavigation } from '@hpapp/features/common/stack';
-import UPFCWebViewScreen from '@hpapp/features/upfc/UPFCWebViewScreen';
+import UPFCWebViewScreen, { UPFCWebViewScreenProps } from '@hpapp/features/upfc/UPFCWebViewScreen';
 import { useCallback } from 'react';
 
 export default function useUPFCWebView() {
   const navigation = useNavigation();
   const openUPFCWeView = useCallback(
-    (params?: { urlParams?: string }) => {
+    (params?: UPFCWebViewScreenProps) => {
       navigation.push(UPFCWebViewScreen, params);
     },
     [navigation]
