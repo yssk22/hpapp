1183867884f6b8a2a7406b2d11e12a323ff67022
yssk22 <yssk22@gmail.com>
2024-11-11 15:34:36 +0000

    [expo] sync upfc data with OS calendar
    **Summary**
    
    we rely on calendar on top of OS calendar feature.
    
    **Test**
    
    - expo
    
    **Issue**
    
    - N/A

--- expo/features/upfc/internals/useUPFCEventApplications.tsx
+++ expo/features/upfc/internals/useUPFCEventApplications.tsx
@@ -10,10 +10,15 @@ import {
   UPFCHttpFetcher,
   UPFCSite,
   UPFCSiteScraper,
-  UPFCCombiedSiteScraper
+  UPFCCombiedSiteScraper,
+  UPFCEventTicket
 } from '@hpapp/features/upfc/scraper';
+import * as date from '@hpapp/foundation/date';
 import { isEmpty } from '@hpapp/foundation/string';
+import * as logging from '@hpapp/system/logging';
+import * as Calendar from 'expo-calendar';
 import { useMemo } from 'react';
+import { Platform } from 'react-native';
 
 type UPFCFetchApplicationsParams = {
   helloproject: {
@@ -25,6 +30,8 @@ type UPFCFetchApplicationsParams = {
     password: string;
   };
   useDemo: boolean;
+  calendarId: string | undefined;
+  eventPrefix: string | undefined;
 };
 
 export type UPFCEventAPplicationsResult = {
@@ -50,9 +57,13 @@ export default function useUPFCEventApplications(): ReloadableAysncResult<
         username: upfcConfig?.mlUsername ?? '',
         password: upfcConfig?.mlPassword ?? ''
       },
-      useDemo: appConfig.useUPFCDemoScraper || upfcConfig?.hpUsername === UPFCDemoScraper.Username
+      useDemo: appConfig.useUPFCDemoScraper || upfcConfig?.hpUsername === UPFCDemoScraper.Username,
+      calendarId: upfcConfig?.calendarId ?? undefined,
+      eventPrefix: upfcConfig?.eventPrefix ?? undefined
     };
   }, [
+    upfcConfig?.calendarId,
+    upfcConfig?.eventPrefix,
     upfcConfig?.hpUsername,
     upfcConfig?.hpPassword,
     upfcConfig?.mlUsername,
@@ -101,7 +112,9 @@ const combinedScraper = new UPFCCombiedSiteScraper([siteScraper2, siteScraper]);
 async function fetchApplications({
   helloproject,
   mline,
-  useDemo
+  useDemo,
+  calendarId,
+  eventPrefix
 }: UPFCFetchApplicationsParams): Promise<UPFCEventAPplicationsResult> {
   if (isEmpty(helloproject?.username) && isEmpty(mline?.username)) {
     throw new ErrUPFCNoCredential();
@@ -110,6 +123,9 @@ async function fetchApplications({
     fetchApplicationsFromSite(helloproject.username, helloproject.password, 'helloproject', useDemo),
     fetchApplicationsFromSite(mline.username, mline.password, 'm-line', useDemo)
   ]);
+  // optionally sync calendar and server in background.
+  const applications = result.flatMap((r) => r.applications);
+  syncUPFC(calendarId, eventPrefix, applications);
 
   return {
     applications: result.flatMap((r) => r.applications),
@@ -160,4 +176,149 @@ function dateOrUndefined(d: Date | undefined): Date | undefined {
     return new Date(d);
   }
   return undefined;
+}
+
+async function syncUPFC(
+  calenderId: string | undefined,
+  eventPrefix: string | undefined,
+  applications: UPFCEventApplicationTickets[]
+): Promise<void> {
+  if (!isEmpty(calenderId)) {
+    await syncToCalender(calenderId!, eventPrefix ?? '', applications);
+  }
+}
+
+type SyncParam = [UPFCEventTicket, string];
+
+async function syncToCalender(
+  calendarId: string,
+  eventPrefix: string,
+  applications: UPFCEventApplicationTickets[]
+): Promise<void> {
+  // sync to calendar
+  // check if the calendarId satisfy the condition.
+  const cal = (await Calendar.getCalendarsAsync()).filter((c) => c.id === calendarId);
+  if (cal.length === 0) {
+    logging.Error('features.upfc.internals.useUPFCEventApplications.syncToCalender', 'cannot find calender id', {
+      calendarId
+    });
+    return;
+  }
+  if (!cal[0].allowsModifications) {
+    logging.Error('features.upfc.internals.useUPFCEventApplications.syncToCalender', 'not allowed', {
+      calendarId
+    });
+    return;
+  }
+  // for Android, getEventsAsync return nothing if the calender is not visible so
+  // users have to select from only visible calendars.
+  if (Platform.OS === 'android' && !cal[0].isVisible) {
+    logging.Error('features.upfc.internals.useUPFCEventApplications.syncToCalender', 'not visible', {
+      calendarId
+    });
+  }
+
+  const dateToEvents: { [key: number]: SyncParam[] } = {};
+  applications.forEach((a) => {
+    a.tickets.forEach((t) => {
+      const d = date.getDate(t.startAt).getTime();
+      if (!dateToEvents[d]) {
+        dateToEvents[d] = [];
+      }
+      dateToEvents[d].push([t, a.name]);
+    });
+  });
+  await Promise.all(
+    Object.values(dateToEvents).map(async (params) => {
+      return await syncToCalendarEvent(params, calendarId, eventPrefix);
+    })
+  );
+}
+
+async function syncToCalendarEvent(params: SyncParam[], calendarId: string, prefix: string) {
+  const start = date.getDate(params[0][0].startAt);
+  const end = date.addDate(start.getTime(), 2, 'day');
+  const events = await Calendar.getEventsAsync([calendarId], start, end);
+  const keyToEvent = events.reduce(
+    (h, e) => {
+      const key = `${e.title}.${date.parseDate(e.startDate).getTime()}.${e.location}`;
+      h[key] = e;
+      return h;
+    },
+    {} as { [key: string]: Calendar.Event }
+  );
+  return Promise.all(
+    params.map(async (p) => {
+      const ticket = p[0];
+      const title = prefix === '' ? p[1] : `${prefix} ${p[1]}`;
+      const location = ticket.venue;
+      const startDate = ticket.openAt ? ticket.openAt : ticket.startAt;
+      const key = `${title}.${date.parseDate(startDate).getTime()}.${location}`;
+      // old app uses event title with '■ ' prefix from the ticket page so we have to take care of.
+      const oldTitle = prefix === '' ? p[1] : `${prefix} ■ ${p[1]}`;
+      const oldKey = `${oldTitle}.${date.parseDate(startDate).getTime()}.${location}`;
+      const calendarEvent = keyToEvent[key] || keyToEvent[oldKey];
+      // TODO: i18n
+      const notes = `${ticket.status} 開演: ${date.toTimeString(
+        ticket.startAt
+      )} (ハロー！ファンにより自動的に追加されました)`;
+      if (calendarEvent) {
+        // 2 possible actions for events which already exist::
+        //   - delete if status gets '落選' or '入金忘'
+        //   - update if title or notes including status are different.
+        if (ticket.status === '落選' || ticket.status === '入金忘') {
+          try {
+            await Calendar.deleteEventAsync(calendarEvent.id);
+            logging.Info('features.upfc.internals.useUPFCEventApplications.syncToCalendarEvent', 'delete', {
+              calendarId
+            });
+          } catch (e) {
+            logging.Error('features.upfc.internals.useUPFCEventApplications.syncToCalendarEvent', 'cannot delete', {
+              calendarId,
+              error: (e as any).toString()
+            });
+          }
+        } else if (notes !== calendarEvent.notes || title !== calendarEvent.title) {
+          try {
+            await Calendar.updateEventAsync(calendarEvent.id, {
+              title,
+              notes
+            });
+            logging.Info('features.upfc.internals.useUPFCEventApplications.syncToCalendarEvent', 'update', {
+              calendarId
+            });
+          } catch (e) {
+            logging.Error('features.upfc.internals.useUPFCEventApplications.syncToCalendarEvent', 'cannot update', {
+              calendarId,
+              error: (e as any).toString()
+            });
+          }
+        }
+      } else {
+        if (ticket.status === '落選' || ticket.status === '入金忘') {
+          return;
+        }
+        const endDate = ticket.openAt
+          ? date.addDate(ticket.openAt.getTime(), 3, 'hour')
+          : date.addDate(ticket.startAt.getTime(), 3, 'hour');
+        try {
+          await Calendar.createEventAsync(calendarId, {
+            title,
+            location,
+            notes,
+            startDate,
+            endDate
+          });
+          logging.Info('features.upfc.internals.useUPFCEventApplications.syncToCalendarEvent', 'create', {
+            calendarId
+          });
+        } catch (e) {
+          logging.Error('features.upfc.internals.useUPFCEventApplications.syncToCalendarEvent', 'cannot create', {
+            calendarId,
+            error: (e as any).toString()
+          });
+        }
+      }
+    })
+  );
 }
