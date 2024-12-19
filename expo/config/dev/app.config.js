b4a2fcee6c16ec192da5302dcf5693ebc84701c9
yssk22 <yssk22@gmail.com>
2024-12-19 03:14:48 +0000

    [expo] update Android configurations
    **Summary**
    
    `dev.yssk22.hpapp` package is not available on Play Console as the developer account was suspended in early 2024.
    I created a new developer account and use `dev.yssk22.hpapp.{env}.android` as a new package name. This change also
    required new client ids for firebase authentication.
    
    For Android, we don't need to use EAS server to build so we use `--local` to build the new android binary to
    save the EAS cost.
    
    **Test**
    
    - `yarn build-android-dev`
    - `yarn build-android-beta`
    
    **Issue**
    
    - #155

--- expo/config/dev/app.config.js
+++ expo/config/dev/app.config.js
@@ -10,13 +10,13 @@ module.exports = (config) => {
   // bundleID used in your Apple Developer Account
   config.ios.bundleIdentifier = 'dev.yssk22.hpapp.dev';
   // package name used in your Goolge Developer Account
-  config.android.package = 'dev.yssk22.hpapp.dev';
+  config.android.package = 'dev.yssk22.hpapp.dev.android';
   config.extra.hpapp.env = 'dev';
   config.extra.hpapp.isBeta = false;
   config.extra.hpapp.isDev = true;
   // those id can be easily found in the URL in the browser in development build so we don't necessary put them into secrets.json
   config.extra.hpapp.firebaseIOSClientID = '265427540400-pq3vj6jg3p1pcrk55odp2v8k98l3jiir.apps.googleusercontent.com';
   config.extra.hpapp.firebaseAndroidClientID =
-    '265427540400-58vl0q3shqllh738b5hb80m34d6deik1.apps.googleusercontent.com';
+    '265427540400-uomdcbi3g35i3242t7pkua8fhtvppnmq.apps.googleusercontent.com';
   return config;
 };
