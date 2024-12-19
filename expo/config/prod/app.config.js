df6f68b1aa9fee31070a3286fbb5147ec3565057
yssk22 <yssk22@gmail.com>
2024-12-19 04:09:58 +0000

    [expo] introduce a new release workflow
    **Summary**
    
    We now use the new release workflow for both iOS and Android.
    
    - all builds are executed on Github Actions instead of eas so that we can reduce the build cost.
    - we are able to select multiple environments (dev, beta, prod) to build the binaries
    - we use eas-{env} for Github Environment to configure secrets.
    - for `dev`, the action automatically archive the build on Github storage so that developers can download and install.
    - for `beta` and `prod` environments, the action automatically submit the build to the store.
    
    **Test**
    
    - Github action with main branch
    
    **Issue**
    
    - #155

--- expo/config/prod/app.config.js
+++ expo/config/prod/app.config.js
@@ -10,14 +10,14 @@ module.exports = (config) => {
   // bundleID used in your Apple Developer Account
   config.ios.bundleIdentifier = 'dev.yssk22.hpapp';
   // package name used in your Goolge Developer Account
-  config.android.package = 'dev.yssk22.hpapp';
+  config.android.package = 'dev.yssk22.hpapp.android';
   config.extra.hpapp.env = 'prd';
   config.extra.hpapp.isBeta = false;
   config.extra.hpapp.isDev = false;
   // those id can be easily found in the URL in the browser in development build so we don't necessary put them into secrets.json
   config.extra.hpapp.firebaseIOSClientID = '265427540400-um42nfj6a2d2i59u7j01pmgpijmqk1oc.apps.googleusercontent.com';
   config.extra.hpapp.firebaseAndroidClientID =
-    '265427540400-83oj9vqhc5o3srj9pvh5d9e9hkfmnmdg.apps.googleusercontent.com';
+    '265427540400-k0bpqfj2fph4vf2sspqd5a85te6lh151.apps.googleusercontent.com';
   config.updates.url = 'https://u.expo.dev/a76e7e46-e508-43cf-93a8-1ae32871d4f7';
   return config;
 };
