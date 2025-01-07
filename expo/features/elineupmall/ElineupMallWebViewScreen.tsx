925d84917eb87a360d2eb06fdc0fba3a44ed92cf
yssk22 <yssk22@gmail.com>
2025-01-07 08:32:05 +0000

    [expo] prefill credentials when opening elineupmall.
    **Summary**
    
    we want to shortcut the process to start checkout.
    
    - when there is an item in the cart, webview will open /cart/ page and open the popup to login with prefilled credentials.
    - users can just tap the login and start checkout.
    
    **Test**
    
    - expo
    
    **Issue**
    
    - #118

--- expo/features/elineupmall/ElineupMallWebViewScreen.tsx
+++ expo/features/elineupmall/ElineupMallWebViewScreen.tsx
@@ -1,13 +1,12 @@
+import { useUPFCConfig } from '@hpapp/features/app/settings';
+import { useThemeColor } from '@hpapp/features/app/theme';
+import { Spacing } from '@hpapp/features/common/constants';
 import { defineScreen, useScreenTitle } from '@hpapp/features/common/stack';
-import { useRef } from 'react';
+import { Button, Icon } from '@rneui/themed';
+import { useCallback, useRef } from 'react';
 import { View, StyleSheet } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { WebView } from 'react-native-webview';
-
-const styles = StyleSheet.create({
-  webviewConainer: {
-    flex: 1
-  }
-});
 
 export type UPFCWebViewScreenProps = {
   login?: boolean;
@@ -18,13 +17,63 @@ export default defineScreen(
   '/elineupmall/wv/',
   function ElineupMallWebView({ login = false, uri }: UPFCWebViewScreenProps) {
     useScreenTitle('Elineup Mall');
+    const insets = useSafeAreaInsets();
+    const [color, contrastColor] = useThemeColor('secondary');
     const webview = useRef<WebView>(null);
+    const config = useUPFCConfig();
+    const username = config?.hpUsername ?? '';
+    const password = config?.hpPassword ?? '';
+    const onLoadEnd = useCallback(() => {
+      webview?.current?.injectJavaScript(`(function(){
+        document.querySelector('#account_info_3 .ty-account-info__buttons a.ty-btn.ty-btn__secondary').click()
+        document.querySelectorAll('input[name="user_login"]').forEach((elem)=> {elem.value=${JSON.stringify(username)}});
+        document.querySelectorAll('input[name="password"]').forEach((elem)=> {elem.value=${JSON.stringify(password)}});
+      })();
+`);
+    }, [uri, username, password, webview]);
+
     return (
       <>
         <View style={styles.webviewConainer}>
-          <WebView ref={webview} source={{ uri }} />
+          <WebView ref={webview} source={{ uri }} onLoadEnd={onLoadEnd} />
+          <View style={[styles.webviewFooter, { paddingBottom: insets.bottom, backgroundColor: contrastColor }]}>
+            <Button
+              type="clear"
+              containerStyle={styles.webviewFooterButton}
+              icon={<Icon name="caretleft" type="antdesign" color={color} />}
+              onPress={() => webview?.current?.goBack()}
+            />
+            <Button
+              type="clear"
+              containerStyle={styles.webviewFooterButton}
+              icon={<Icon name="caretright" type="antdesign" color={color} />}
+              onPress={() => webview?.current?.goForward()}
+            />
+            <Button
+              type="clear"
+              containerStyle={styles.webviewFooterButton}
+              icon={<Icon name="reload1" type="antdesign" color={color} />}
+              onPress={() => webview?.current?.reload()}
+            />
+          </View>
         </View>
       </>
     );
   }
 );
+
+const styles = StyleSheet.create({
+  webviewConainer: {
+    flex: 1
+  },
+  webviewMain: {
+    flex: 1
+  },
+  webviewFooter: {
+    flexDirection: 'row',
+    paddingTop: Spacing.XSmall
+  },
+  webviewFooterButton: {
+    flexGrow: 1
+  }
+});
