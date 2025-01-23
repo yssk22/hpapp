26238082e666d8f3a428b54668f67c12181b0745
yssk22 <yssk22@gmail.com>
2025-01-23 16:12:36 +0000

    [expo] add a tool to copy the token to clipboard
    **Summary**
    
    we want to copy access token to clipboard to do some debugging in production/beta binary.
    
    **Test**
    
    - expo
    
    **Issue**
    
    - N/A

--- expo/features/devtool/internals/DevtoolListItem.tsx
+++ expo/features/devtool/internals/DevtoolListItem.tsx
@@ -1,4 +1,6 @@
 import { ListItemKeyValue } from '@hpapp/features/common/list';
+import * as Clipboard from 'expo-clipboard';
+import Toast from 'react-native-root-toast';
 
 export default function DevtoolListItem({
   name,
@@ -18,6 +20,8 @@ export default function DevtoolListItem({
         // intentional console output so that developer can copy on their terminal.
         // eslint-disable-next-line no-console
         console.log('Settings', name, ':', value);
+        Clipboard.setStringAsync(value);
+        Toast.show('copied to clipboard');
       }}
     />
   );
