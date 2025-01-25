24fe70aed01594342fc016d710c9be64f1214a59
yssk22 <yssk22@gmail.com>
2025-01-25 08:24:59 +0000

    [expo] update Android package name
    **Summary**
    
    dev.yssk22.hpapp.android was moved to the organization developer account but it was still being blocked by 12 testers policy.
    
    Then I created a new app in the org dev account, it is not being blocked so it looks like the policy doesn't appliy for apps created by the org account.
    
    To use apps in the org account, we need new package names.
    
    **Test**
    
    - N/A
    
    **Issue**
    
    - N/A

--- expo/config/beta/app.config.js
+++ expo/config/beta/app.config.js
@@ -10,13 +10,13 @@ module.exports = (config) => {
   // bundleID used in your Apple Developer Account
   config.ios.bundleIdentifier = 'dev.yssk22.hpapp.beta';
   // package name used in your Goolge Developer Account
-  config.android.package = 'dev.yssk22.hpapp.beta.android';
+  config.android.package = 'dev.yssk22.hellofanapp.beta';
   config.extra.hpapp.env = 'beta';
   config.extra.hpapp.isBeta = true;
   config.extra.hpapp.isDev = false;
   config.extra.hpapp.firebaseIOSClientID = '265427540400-kbuq50tafej5vhrtfdldi6ob61o0jrcu.apps.googleusercontent.com';
   config.extra.hpapp.firebaseAndroidClientID =
-    '265427540400-hl4b6t82h8d9jg7eemovlsftrn0c6tog.apps.googleusercontent.com';
+    '265427540400-s1mtg9uipl4vrbglmegoui9lli5mnb32.apps.googleusercontent.com';
 
   config.updates.url = 'https://u.expo.dev/a76e7e46-e508-43cf-93a8-1ae32871d4f7';
   return config;
